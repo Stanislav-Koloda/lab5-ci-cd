5fda9e76c1daf0b497e44f22b36a0c05102e165d
Stanislav <stanislav.koloda@oa.edu.ua>
2025-05-04 23:15:50 +0300

    Update Vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
+  base: '/lab5-ci-cd/', // назва вашого репозиторію
 });
